950540f3249cc84c4df0dae2eea967cf1825595f
KatherineStrider <kotopesska112@yandex.ru>
2017-05-10 00:45:31 +0400

    Методы onCreate и getInstance для NewShoppingItem

--- app/src/main/java/com/example/kate/shoppinglist/fragments/NewShoppingItem.java
+++ app/src/main/java/com/example/kate/shoppinglist/fragments/NewShoppingItem.java
@@ -1,6 +1,13 @@
 package com.example.kate.shoppinglist.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kate.shoppinglist.R;
 
 /**
  * Created by Kate on 09.05.2017.
@@ -8,5 +15,20 @@ import android.support.v4.app.Fragment;
 
 public class NewShoppingItem extends Fragment {
 
-    
+
+    public static final String TAG = NewShoppingItem.class.getSimpleName();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.view_shop_list, container, false);
+
+        return view;
+    }
+
+    public static NewShoppingItem getInstance() {
+        NewShoppingItem fragment = new NewShoppingItem();
+        return fragment;
+    }
 }
