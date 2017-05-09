a45727262834a232f2ed211a51b2f1278901dd0e
KatherineStrider <kotopesska112@yandex.ru>
2017-05-10 00:34:23 +0400

    Добавление фрагмента для нового элемента списка покупок

---
+++ app/src/main/java/com/example/kate/shoppinglist/fragments/NewShoppingItem.java
@@ -0,0 +1,12 @@
+package com.example.kate.shoppinglist.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Kate on 09.05.2017.
+ */
+
+public class NewShoppingItem extends Fragment {
+
+    
+}
