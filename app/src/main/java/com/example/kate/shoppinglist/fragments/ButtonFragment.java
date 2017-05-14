dfb11e882657c0b172c318dffe9d330e65dd30a4
KatherineStrider <kotopesska112@yandex.ru>
2017-05-14 18:02:41 +0400

    Добавление фрагмента для скрытия нижней панели с добавлением нвого элемента

---
+++ app/src/main/java/com/example/kate/shoppinglist/fragments/ButtonFragment.java
@@ -0,0 +1,8 @@
+package com.example.kate.shoppinglist.fragments;
+
+/**
+ * Created by Kate on 14.05.2017.
+ */
+
+public class ButtonFragment {
+}
---
+++ app/src/main/java/com/example/kate/shoppinglist/interfaces/ChangeFragment.java
@@ -0,0 +1,10 @@
+package com.example.kate.shoppinglist.interfaces;
+
+/**
+ * Created by Kate on 14.05.2017.
+ */
+
+public interface ChangeHeightFragment {
+
+    void changeHeight();
+}
